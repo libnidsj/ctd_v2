ef52e0da1b1fe54e559278175379f99130a5a227
Líbni Junior <libnidsj@gmail.com>
2023-04-20 09:12:28 -0300

    Update Decod7seg.vhd

--- Decod7seg.vhd
+++ Decod7seg.vhd
@@ -1,24 +1,20 @@
-library IEEE;
-use IEEE.Std_Logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL; USE ieee.numeric_std.ALL;
 
-entity decod7seg is port (
+ENTITY decodificadorBCD7Seg IS PORT (
+	bcd : IN std_logic_vector(3 DOWNTO 0);
+	a, b, c, d, e, f, g : OUT std_logic );
+END decodificadorBCD7Seg;
+	
+ARCHITECTURE arch OF decodificadorBCD7Seg IS
+BEGIN
+	
+	a <= bcd[0] or bcd[2] or (bcd[1] and bcd[3]) or ((not bcd[1]) and (not bcd[3]));
+	b <= (not bcd[1]) or ((not bcd[2]) and (not bcd[3])) or (bcd[2] and bcd[3]);
+	c <= bcd[1] or (not bcd[2]) or bcd[3];
+	d <= ((not bcd[1]) and (not bcd[3])) or (bcd[2] and (not bcd[3])) or (bcd[1] and (not bcd[2]) and bcd[3]) or ((not bcd[1]) and bcd[2]) or (bcd[0]);
+	e <= ((not bcd[1]) and (not bcd[3])) or (bcd[2] and (not bcd[3]));
+	f <= (bcd[0]) or ((not bcd[2]) and (not bcd[3])) or (bcd[1] and (not bcd[2])) or (bcd[1] and (not bcd[3]));
+	g <= (bcd[0]) or (bcd[1] and (not bcd[2])) or ((not bcd[1]) and bcd[2]) or (bcd[2] and (not bcd[3]));
 
-	input  : in  std_logic_vector(3 downto 0);
-	output : out std_logic_vector(6 downto 0));
-
-end decod7seg;
-
-architecture arc_decod7seg of decod7seg is begin
-
-	output <= "1000000" when input = "0000" else -- 0
-			    "1111001" when input = "0001" else -- 1
-		       "0100100" when input = "0010" else -- 2
-		       "0110000" when input = "0011" else -- 3
-			    "0011001" when input = "0100" else -- 4
-			    "0010010" when input = "0101" else -- 5
-			    "0000010" when input = "0110" else -- 6
-			    "1111000" when input = "0111" else -- 7
-			    "0000000" when input = "1000" else -- 8
-			    "0010000";
-
-end arc_decod7seg;
+END ARCHITECTURE; 
